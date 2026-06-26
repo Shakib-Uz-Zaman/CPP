fdd12370a1031f1762ecf5bc2152ae4f409e2f95
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-06-27 00:03:56 +0600

    cout<<showpoint

--- programs6.cpp
+++ programs6.cpp
@@ -3,24 +3,28 @@
 using namespace std;
 int main()
 {
-   int num1, num2;
+   float num1, num2;
    cout<< "Enter two numbers: ";
    cin>> num1 >> num2; 
 
-   int sum = num1 + num2;
+
+   cout<<showpoint;
+
+   float sum = num1 + num2;
    cout<< "Sum:"<< sum <<endl;
 
-   int Biyog = num1 - num2;
+   float Biyog = num1 - num2;
    cout<< "Biyog: "<< Biyog<<endl;
 
-   int Gun = num1 * num2;
+   float Gun = num1 * num2;
    cout<< "Gun: "<< Gun<<endl;
+
 
    float Vag = (float) num1 / num2;
    cout<< "Vag: "<< Vag<<endl;
 
-   int Vagses = num1 % num2;
-   cout<< "Vagses: "<< Vagses<<endl;
+//    int Vagses = num1 % num2;
+//    cout<< "Vagses: "<< Vagses<<endl;
 
     getch();
 }
