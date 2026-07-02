382123cb7b933bda5f40e2f10518e31ce6331082
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-02 20:01:43 +0600

    / 1 + 2 + 3 + 4 ...... + n

---
+++ series_1.cpp
@@ -0,0 +1,19 @@
+// 1 + 2 + 3 + 4 ...... + n
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter last number :";
+    cin>>n;
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    cout<<"Sum is : "<<sum;
+
+    getch();
+}
